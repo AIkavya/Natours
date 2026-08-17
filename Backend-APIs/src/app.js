b50664d62b1e503f16c25c8092bf91875f6df66f
Kavya Panchal <kavya2310.kp@gmail.com>
2026-08-17 11:46:47 +0530

    Update app.js

--- Backend-APIs/src/app.js
+++ Backend-APIs/src/app.js
@@ -31,6 +31,7 @@ const cors = require("cors");
 
 const allowedOrigins = [
   process.env.FRONTEND_URL,
+  "https://natours-hap0ciags-kavya-043d.vercel.app",
   "http://localhost:5173",
   "http://localhost:3000",
   "http://127.0.0.1:5173",
